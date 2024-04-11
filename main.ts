0a6d72206cbc9d382bdd21bc225c22e9954dd592
dasdasdashdfd233464 <166718951+dasdasdashdfd233464@users.noreply.github.com>
2024-04-11 15:55:35 +0300

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
-let A = 0
-let AB = 0
 input.onButtonPressed(Button.A, function () {
-    A += A + 1
+    A += 1
     basic.showNumber(A)
 })
 input.onButtonPressed(Button.AB, function () {
-    AB += 0
+    A = 0
     basic.showNumber(A)
 })
 input.onButtonPressed(Button.B, function () {
-    A += A - 1
+    A += -1
     basic.showNumber(A)
+})
+let A = 0
+A = 0
+basic.forever(function () {
+    if (A >= 9) {
+        A = 0
+        basic.showNumber(A)
+    }
 })
